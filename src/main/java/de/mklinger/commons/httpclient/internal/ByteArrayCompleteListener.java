f61729b2f34f37d8770cb7e9c70ef4298d9fa1e1
Marc Klinger <mklinger@mklinger.de>
2018-09-03 13:30:11 +0200

    Use synchronized in byte array complete listener
    Be sure to have clean memory barriers when manipulating internal array
    list. Build final byte array on demand.

--- src/main/java/de/mklinger/commons/httpclient/internal/ByteArrayCompleteListener.java
+++ src/main/java/de/mklinger/commons/httpclient/internal/ByteArrayCompleteListener.java
@@ -10,36 +10,35 @@ import de.mklinger.commons.httpclient.HttpResponse;
  * @author Marc Klinger - mklinger[at]mklinger[dot]de
  */
 public class ByteArrayCompleteListener implements HttpResponse.BodyCompleteListener<byte[]> {
-	private volatile List<byte[]> byteArrays;
-	private volatile byte[] body;
+	private final List<byte[]> byteArrays = new ArrayList<>();
 
 	@Override
 	public void onNext(final ByteBuffer content) throws Exception {
-		if (byteArrays == null) {
-			byteArrays = new ArrayList<>();
-		}
 		final int len = content.remaining();
 		final byte[] dst = new byte[len];
 		content.get(dst);
-		byteArrays.add(dst);
+		synchronized (byteArrays) {
+			byteArrays.add(dst);
+		}
 	}
 
 	@Override
 	public void onComplete() throws Exception {
-		if (byteArrays == null) {
-			body = new byte[0];
-		} else {
-			body = concat(byteArrays);
-			byteArrays = null;
-		}
+		// Do nothing
 	}
 
 	@Override
 	public byte[] getBody() throws Exception {
-		return body;
+		synchronized (byteArrays) {
+			return concat(byteArrays);
+		}
 	}
 
 	private byte[] concat(final List<byte[]> byteArrays) {
+		if (byteArrays.isEmpty()) {
+			return new byte[0];
+		}
+
 		int fullLen = 0;
 		for (final byte[] bs : byteArrays) {
 			fullLen += bs.length;
@@ -58,7 +57,8 @@ public class ByteArrayCompleteListener implements HttpResponse.BodyCompleteListe
 
 	@Override
 	public void close() throws Exception {
-		byteArrays = null;
-		body = null;
+		synchronized (byteArrays) {
+			byteArrays.clear();
+		}
 	}
 }
